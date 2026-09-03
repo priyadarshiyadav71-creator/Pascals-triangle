6a2c844d514b113fd91439b0e6c41117ae5e8252
priyadarshiyadav71-creator <priyadarshiyadav71@gmail.com>
2026-09-03 11:17:57 +0530

    Add files via upload

---
+++ pascal triangle/script.js
@@ -0,0 +1,32 @@
+const inputValue = document.querySelector("input")
+const para = document.querySelector("p")
+const btn = document.querySelector("button")
+
+function factorial(n) {
+    if (n < 0) throw new Error("Negative numbers not allowed");
+    if (n === 0 || n === 1) return 1;
+    let result = 1;
+    for (let i = 2; i <= n; i++) result *= i;
+    return result;
+}
+
+function getRowCoefficients(n) {
+    const row = []
+    for (i = 0; i <= n; i++) {
+        const formula = factorial(n) / (factorial(i) * factorial(n - i))
+        row.push(formula)
+    }
+    return row
+}
+
+btn.addEventListener("click", () => {
+    if (inputValue.value === "") return
+    let outputString = ""
+    const maxRows = Number(inputValue.value)
+    for(let n = 0; n <= maxRows; n++){
+        const rowData = getRowCoefficients(n)
+        outputString += rowData.join(" ") + "<br>"
+    }
+    para.innerHTML = outputString
+    inputValue.value = ""
+})
